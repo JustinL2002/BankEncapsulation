dfa1b7c1eca0c405b6daa124c61426324241e63a
Justin Lawson <jmlawson2002@gmail.com>
2022-09-23 14:17:14 -0400

    Encapsulation excercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using BankEncapsulation;
+{
+    BankAccount ba = new BankAccount();
+
+    Console.WriteLine("How much money do you want to deposit?");
+
+    double depositAmount = double.Parse(Console.ReadLine());
+
+    ba.Deposit(depositAmount);
+    double userBalance = ba.GetBalance();
+
+    Console.WriteLine($"Your balance is {userBalance}");
+}
